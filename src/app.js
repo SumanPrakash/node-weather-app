61bfab8e5ca2b66410db76c4f80d352c0b0be4a0
Suman Prakash <suman@Sumans-MacBook-Pro.local>
2020-05-06 01:07:58 +0530

    Setup app for Heroku

--- src/app.js
+++ src/app.js
@@ -5,6 +5,7 @@ const hbs = require('hbs');
 const getWeatherUpdate = require('./utils/weatherForecast');
 
 const app = express();
+const port = process.env.PORT || 3000;
 
 // Define paths for express config
 const publicDirectoryPath = path.join(__dirname, '../public');
@@ -72,6 +73,6 @@ app.get('*', (req, res) => {
   });
 });
 
-app.listen(3000, () => {
-  console.log('Server started at port 3000');
+app.listen(port, () => {
+  console.log(`Server started at port ${port}`);
 });
